e4c55694c83fcd5f9053e2d1fce1037984f2401e
BANSARI MISTRY <bansari.mistry.serpentcs@gmail.com>
2018-07-10 17:46:57 +0530

    [MIG] product_rating: Migrated the module from v08 to v10.

--- product_rating/__manifest__.py
+++ product_rating/__manifest__.py
@@ -4,30 +4,22 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Product Rating",
-    "summary": "Rate your product",
-    "version": "8.0.1.0.0",
-    "category": "Uncategorized",
-    "website": "www.akretion.com",
-    "author": " Akretion",
-    "license": "AGPL-3",
-    "application": False,
+    'name': 'Product Rating',
+    'version': '10.0.1.0.0',
+    'author': "Akretion, "
+              "Serpent Consulting Services Pvt. Ltd.",
+    'website': 'http://www.akretion.com',
+    'license': 'AGPL-3',
+    'category': 'Uncategorized',
+    'summary': 'Rate your product',
+    'depends': [
+        'sales_team', 'product'
+    ],
+    'data': [
+        'views/product_rating_view.xml',
+        'views/product_view.xml',
+        'data/product_rating_data.xml',
+        'security/ir.model.access.csv',
+    ],
     'installable': False,
-    "external_dependencies": {
-        "python": [],
-        "bin": [],
-    },
-    "depends": [
-        "product",
-    ],
-    "data": [
-        "views/product_rating_view.xml",
-        "views/product_view.xml",
-        "data/product_rating_data.xml",
-        "security/ir.model.access.csv",
-    ],
-    "demo": [
-    ],
-    "qweb": [
-    ]
 }

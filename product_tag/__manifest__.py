5431856c816e858bb923e868bec66c72b8217aea
BANSARI MISTRY <bansari.mistry.serpentcs@gmail.com>
2018-07-10 19:18:52 +0530

    [MIG] product_tag : Migrated the module from v8 to v10.

--- product_tag/__manifest__.py
+++ product_tag/__manifest__.py
@@ -4,29 +4,21 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Product Tag",
-    "summary": "Tag your product",
-    "version": "8.0.1.0.0",
-    "category": "E-commerce",
-    "website": "www.akretion.com",
-    "author": " Akretion",
-    "license": "AGPL-3",
-    "application": False,
+    'name': 'Product Tag',
+    'version': '10.0.1.0.0',
+    'author': "Akretion, "
+              "Serpent Consulting Services Pvt. Ltd.",
+    'website': 'http://www.akretion.com',
+    'license': 'AGPL-3',
+    'category': 'E-commerce',
+    'summary': 'Tag your product',
+    'depends': [
+        'product', 'sales_team'
+    ],
+    'data': [
+        'views/product_tag_view.xml',
+        'views/product_view.xml',
+        'security/ir.model.access.csv',
+    ],
     'installable': False,
-    "external_dependencies": {
-        "python": [],
-        "bin": [],
-    },
-    "depends": [
-        "product",
-    ],
-    "data": [
-        "views/product_tag_view.xml",
-        "views/product_view.xml",
-        "security/ir.model.access.csv",
-    ],
-    "demo": [
-    ],
-    "qweb": [
-    ]
 }
